0c85ab6b857c3060fa9015bb8020f0acbfd181d2
BitTim <tim.anhalt.2004@protonmail.com>
2020-07-18 12:19:36 +0200

    Added Inventory Tooltips

--- Scripts/GUI/Inventory/Item.gd
+++ Scripts/GUI/Inventory/Item.gd
@@ -6,5 +6,5 @@ func _ready():
 	pass
 
 func setType(itemName):
-	$Tween.interpolate_property($Sprite, "frame", def.ITEM_DATA[itemName].startFrame, def.ITEM_DATA[itemName].startFrame + (def.ITEM_DATA[itemName].numFrames - 1), 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 1)
+	$Tween.interpolate_property($Sprite, "frame", def.ITEM_DATA[itemName].startFrame, def.ITEM_DATA[itemName].startFrame + (def.ITEM_DATA[itemName].numFrames - 1), 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 1 / def.ITEM_DATA[itemName].numFrames)
 	$Tween.start()
