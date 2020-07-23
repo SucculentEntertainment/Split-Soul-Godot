e14d6a87d2167e45dae99410799d0b98e3a3cacb
BitTim <tim.anhalt.2004@protonmail.com>
2020-07-23 23:41:21 +0200

    Added Loading Screen Messages

--- Scripts/GUI/LoadingScreen.gd
+++ Scripts/GUI/LoadingScreen.gd
@@ -3,7 +3,13 @@ extends Control
 signal loadingFinished
 onready var def = get_node("/root/Definitions")
 
+var rng = RandomNumberGenerator.new()
+
 func start(variant, level, gui):
+	rng.randomize()
+	var i = rng.randi_range(0, def.LOADING_SCREEN_MESSAGES.size() - 1)
+	$VBoxContainer/Label.text = def.LOADING_SCREEN_MESSAGES[i]
+	
 	show()
 	$AnimationPlayer.play(variant)
 	
