2463b06b80e57a0475cf440176dad8e22f062552
BitTim <tim.anhalt.2004@protonmail.com>
2020-07-25 13:43:27 +0200

    Moved Spawnable scenes to JSON and started LevelChange

--- Scripts/Definitions.gd
+++ Scripts/Definitions.gd
@@ -19,25 +19,7 @@ var PLAYER_SCENE = preload("res://Scenes/Entities/Player.tscn")
 var ITEM_SCENE = preload("res://Scenes/GUI/Inventory/Item.tscn")
 var TRANSITION_SCENE = preload("res://Scenes/Effects/TransitionShader.tscn")
 
-var SPAWNABLE_SCENES = {
-	"t_tile": preload("res://Scenes/Entities/Tile.tscn"),
-	
-	"o_oakTree1": preload("res://Scenes/Entities/Objects/OakTree1.tscn"),
-	"o_pineTree": preload("res://Scenes/Entities/Objects/PineTree.tscn"),
-	"o_birchTree": preload("res://Scenes/Entities/Objects/BirchTree.tscn"),
-	"o_bush": preload("res://Scenes/Entities/Objects/Bush.tscn"),
-	"o_oakTree2": preload("res://Scenes/Entities/Objects/OakTree2.tscn"),
-	
-	"e_slime": preload("res://Scenes/Entities/Enemies/Slime.tscn"),
-	
-	"n_altar": preload("res://Scenes/Entities/Interactables/Altar.tscn"),
-	
-	"p_coin": preload("res://Scenes/Entities/Powerups/Coin.tscn"),
-	"p_soulPoint": preload("res://Scenes/Entities/Powerups/SoulPoint.tscn"),
-	"p_itemStack": preload("res://Scenes/Entities/Powerups/ItemStack.tscn"),
-	
-	"g_spawn": preload("res://Scenes/Entities/Triggers/SpawnTrigger.tscn")
-}
+var SPAWNABLE_SCENES = {}
 
 # ================================
 # Dimensions
@@ -72,3 +54,9 @@ func _ready():
 	
 	file.open("res://Data/loadingScreenMessages.json", file.READ)
 	LOADING_SCREEN_MESSAGES = parse_json(file.get_as_text())
+	
+	file.open("res://Data/spawnableScenes.json", file.READ)
+	var scenes = parse_json(file.get_as_text())
+	
+	for scene in scenes:
+		SPAWNABLE_SCENES[scene.id] = load(scene.scene)
