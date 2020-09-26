b0878ae2012f0809066ad3e6058666e657274793
BitTim <tim.anhalt.2004@protonmail.com>
2020-09-26 11:25:05 +0200

    Added DarkMode to BootLogo

--- Scripts/BootSplash.gd
+++ Scripts/BootSplash.gd
@@ -3,14 +3,24 @@ extends ColorRect
 onready var def = get_node("/root/Definitions")
 export (bool) var skipLogo = false
 export (bool) var forceAlt = false
+export (bool) var darkMode = false
 
 export (Texture) var altIcon
 export (Texture) var altLogo
+
+export (Color)   var darkModeBG
+export (Texture) var darkModeText
+export (Texture) var darkModeAltLogo
 
 var rng = RandomNumberGenerator.new()
 
 func _ready():
 	OS.window_fullscreen = def.CONFIG.get_value("video", "fullscreen")
+	
+	if darkMode:
+		color = darkModeBG
+		$Logo.texture = darkModeText
+		altLogo = darkModeAltLogo
 	
 	rng.randomize()
 	var a = rng.randi_range(0, 100)
